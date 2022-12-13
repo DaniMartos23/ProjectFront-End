57d2bb563a1438ff5a58198adc8ac538cac7d1c5
Oscar herrán <114735975+OscarHerran@users.noreply.github.com>
2022-12-13 11:53:21 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { ElementsModule } from './elements/elements.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
