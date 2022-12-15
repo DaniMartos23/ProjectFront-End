446454e2e45909ebe4abafea4496759fb5909263
Oscar herrán <114735975+OscarHerran@users.noreply.github.com>
2022-12-15 22:20:55 +0100

    updates footer

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
@@ -13,7 +14,8 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   exports:[
     NavbarComponent,
