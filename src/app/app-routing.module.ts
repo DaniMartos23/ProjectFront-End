512e190e45c463a798a1a8998839726f1b130150
Oscar herrán <114735975+OscarHerran@users.noreply.github.com>
2022-12-17 09:04:42 +0100

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrudHotelesComponent } from './crud-hoteles/crud-hoteles.component';
 import { LoginsComponent } from './logins/logins.component';
 
+
 const routes: Routes = [
-  {
-    path:'logins',
-    component:LoginsComponent
-  }
+  {path:'logins',component:LoginsComponent},
+  {path:'crudhoteles',component:CrudHotelesComponent}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
+
+import { HttpClientModule } from '@angular/common/http';
 import { ElementsModule } from './elements/elements.module';
 import { LoginsModule } from './logins/logins.module';
+//componentes
+import { AddEditHotelsComponent } from './crud-hoteles/add-edit-hotels/add-edit-hotels.component';
+import { ListHotelsComponent } from './crud-hoteles/listhotels/listhotels.component';
+import { SeehotelsComponent } from './crud-hoteles/seehotels/seehotels.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEditHotelsComponent,
+    ListHotelsComponent,
+    SeehotelsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ElementsModule,
     LoginsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
