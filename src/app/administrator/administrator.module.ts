fe7dc499e6fb11bbb4b2dd72d2f97f35a1a98b4f
Dani Martos <dani.martos69@gmail.com>
2022-12-20 22:42:49 +0100

    cambios admin

--- src/app/administrator/administrator.module.ts
+++ src/app/administrator/administrator.module.ts
@@ -6,27 +6,29 @@ import { AdminviajesComponent } from './adminviajes/adminviajes.component';
 import { AdminvuelosComponent } from './adminvuelos/adminvuelos.component';
 import { AdminhotelesComponent } from './adminhoteles/adminhoteles.component';
 import { FormsModule } from '@angular/forms';
+import { ElementsModule } from "../elements/elements.module";
 
 
 
 @NgModule({
-  declarations: [
-    AdministratorComponent,
-    AdmincholloComponent,
-    AdminviajesComponent,
-    AdminvuelosComponent,
-    AdminhotelesComponent
-  ],
-  imports: [
-    CommonModule,
-    FormsModule
-  ],
-  exports:[
-    AdministratorComponent,
-    AdmincholloComponent,
-    AdminviajesComponent,
-    AdminvuelosComponent,
-    AdminhotelesComponent
-  ]
+    declarations: [
+        AdministratorComponent,
+        AdmincholloComponent,
+        AdminviajesComponent,
+        AdminvuelosComponent,
+        AdminhotelesComponent
+    ],
+    exports: [
+        AdministratorComponent,
+        AdmincholloComponent,
+        AdminviajesComponent,
+        AdminvuelosComponent,
+        AdminhotelesComponent
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ElementsModule
+    ]
 })
 export class AdministratorModule { }
