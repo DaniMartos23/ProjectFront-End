01e96209d976617b0d8f64d0eea56bc51a73dc3d
Dani Martos <dani.martos69@gmail.com>
2022-12-22 00:22:15 +0100

    modificacion final admin

--- src/app/administrator/adminhoteles/adminhoteles.component.ts
+++ src/app/administrator/adminhoteles/adminhoteles.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { GeneralesService } from 'src/app/services/generales.service';
 
 @Component({
@@ -8,6 +9,7 @@ import { GeneralesService } from 'src/app/services/generales.service';
 })
 export class AdminhotelesComponent {
   menu:number=1;
+  mod:boolean=false;
   datos:any;
   id:any;
   form: any = {
@@ -23,26 +25,83 @@ export class AdminhotelesComponent {
 
   constructor(private serviciosgenerales: GeneralesService){}
 
-  onSubmit(){
+  onSubmit(fcrear:NgForm){
 
     this.serviciosgenerales.creardatos("hoteles",this.form)
       .subscribe(result =>this.datos=result)
       this.menu=1;
+      fcrear.resetForm();
   }
   mostrarmenu(numero:any){
     this.menu=numero;
     this.datos=null;
   }
 
-  buscahotel(){
-    this.serviciosgenerales.retornadato("hoteles",this.id)
-      .subscribe(result =>this.datos=result)
-      this.menu=1;
+  mostrarhoteles() {
+    this.menu = 2;
+    this.mod = false;
+    this.serviciosgenerales
+      .retornardatos('hoteles')
+      .subscribe((result) => (this.datos = result));
   }
 
-  borrahotel(){
-    this.serviciosgenerales.deletedatos("hoteles",this.id)
-      .subscribe(result =>this.datos=result)
-      this.menu=1;
+  buscahoteles() {
+    this.serviciosgenerales
+      .retornadato('hoteles', this.id)
+      .subscribe((result) => (this.datos = result));
+    this.menu = 1;
+  }
+
+  borrarhoteles(ids: number) {
+    this.serviciosgenerales
+      .deletedatos('hoteles', ids)
+      .subscribe((result) => (this.datos = result));
+    this.menu = 1;
+  }
+  modificahoteles(ids: number) {
+    this.mod = true;
+    this.serviciosgenerales
+      .retornadato('hoteles', ids)
+      .subscribe((result) => (this.datos = result));
+  }
+  modifica(fmodificar: NgForm) {
+
+    if (this.form.nombre == null) {
+      this.form.nombre = this.datos.nombre;
+    }
+    if (this.form.direccion == null) {
+      this.form.direccion = this.datos.direccion;
+    }
+    if (this.form.descripcion == null) {
+      this.form.descripcion = this.datos.descripcion;
+    }
+    if (this.form.localidad == null) {
+      this.form.localidad = this.datos.localidad;
+    }
+    if (this.form.estrellas == null) {
+      this.form.estrellas = this.datos.estrellas;
+    }
+
+    if (this.form.tipo_habitaciones == null) {
+      this.form.tipo_habitaciones = this.datos.tipo_habitaciones;
+    }
+
+    if (this.form.mascotas == null) {
+      this.form.mascotas = this.datos.mascotas;
+    }
+
+    if (this.form.tipo_pension == null) {
+      this.form.tipo_pension = this.datos.tipo_pension;
+    }
+
+
+    this.serviciosgenerales
+      .updatedatos('hoteles', this.datos.id, this.form)
+      .subscribe((result) => {
+        this.datos = result;
+        this.mostrarhoteles();
+        fmodificar.resetForm();
+      });
   }
 }
+
--- src/app/administrator/adminviajes/adminviajes.component.ts
+++ src/app/administrator/adminviajes/adminviajes.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { GeneralesService } from 'src/app/services/generales.service';
 import { UsuariosService } from 'src/app/services/usuarios.service';
 
@@ -7,8 +8,9 @@ import { UsuariosService } from 'src/app/services/usuarios.service';
   templateUrl: './adminviajes.component.html',
   styleUrls: ['./adminviajes.component.css'],
 })
-export class AdminviajesComponent {
+export class AdminviajesComponent implements OnInit {
   menu: number = 1;
+  mod: boolean = false;
   datos: any;
   id: any;
   form: any = {
@@ -28,9 +30,11 @@ export class AdminviajesComponent {
   constructor(
     private serviciosgenerales: GeneralesService,
     private usuariosservice: UsuariosService
-  ) {}
-
-  onSubmit() {
+  ) { }
+  ngOnInit(): void {
+    this.mod = false;
+  }
+  onSubmit(fcrear: NgForm) {
     this.serviciosgenerales
       .retornadato('vuelos', this.form.vuelos)
       .subscribe((result) => {
@@ -46,32 +50,85 @@ export class AdminviajesComponent {
             console.log(this.Hoteles);
 
             this.datos = this.form;
-            this.form.viajes = this.Vuelos;
-            this.form.usuario = this.Hoteles;
-            console.log(this.form);
-
+            this.form.vuelos = this.Vuelos;
+            this.form.hotel = this.Hoteles;
             this.serviciosgenerales
-              .creardatos('viajes', this.form)
-              .subscribe((result) => (this.datos = result));
-            this.menu = 0;
+              .creardatos('viajes/add', this.form)
+              .subscribe((result) => {
+                (this.datos = result)
+                fcrear.resetForm();
+                this.menu = 0;
+              });
+
+
           });
       });
   }
   mostrarmenu(numero: any) {
     this.menu = numero;
   }
+  mostrarviajes() {
+    this.menu = 2;
+    this.mod = false;
 
-  buscaviaje() {
+    this.serviciosgenerales
+      .retornardatos('viajes')
+      .subscribe((result) => (this.datos = result));
+  }
+
+  buscaviajes() {
     this.serviciosgenerales
       .retornadato('viajes', this.id)
       .subscribe((result) => (this.datos = result));
     this.menu = 1;
   }
 
-  borraviaje() {
+  borrarviajes(ids: number) {
     this.serviciosgenerales
-      .deletedatos('viajes', this.id)
+      .deletedatos('viajes', ids)
       .subscribe((result) => (this.datos = result));
     this.menu = 1;
+  }
+  modificaviajes(ids: number) {
+    this.mod = true;
+    this.serviciosgenerales
+      .retornadato('viajes', ids)
+      .subscribe((result) => (this.datos = result));
+  }
+  modifica(fmodificar: NgForm) {
+
+    if (this.form.vuelos == null) {
+      this.Vuelos.id = this.datos.vuelos;
+      this.form.vuelos=this.Vuelos;
+    }
+    else{
+      this.Vuelos.id=this.form.vuelos;
+      this.form.vuelos=this.Vuelos;
+    }
+    if (this.form.hotel == null) {
+      this.Hoteles.id = this.datos.hotel.id;
+      this.form.hotel=this.Hoteles;
+    }
+    else{
+      this.Hoteles.id=this.form.hotel;
+      this.form.hotel=this.Hoteles;
+    }
+    if (this.form.origen == null) {
+      this.form.origen = this.datos.origen;
+    }
+    if (this.form.destino == null) {
+      this.form.destino = this.datos.destino;
+    }
+
+
+    this.serviciosgenerales
+      .updatedatos('viajes', this.datos.id, this.form)
+      .subscribe((result) => {
+        this.datos = result;
+        this.mostrarviajes();
+        fmodificar.resetForm();
+      });
+
+
   }
 }
